47c1444e75ad883c953a5b47f1d16944a1a99050
artyhacker <artyhacker@outlook.com>
2021-02-23 09:56:19 +0800

    chore: test SearchBar

--- src/components/Demo/DemoSearch.tsx
+++ src/components/Demo/DemoSearch.tsx
@@ -2,12 +2,17 @@ import { FC } from "react";
 import SearchBar from "../SearchBar";
 
 const DemoSearch: FC = () => {
-  return <SearchBar items={[
-    { name: 'name', label: '名称' },
-    { name: 'code', label: '编码' },
-    { name: 'name1', label: '名称1' },
-    { name: 'name2', label: '名称2' },
-  ]} />
+  return (
+    <SearchBar
+      items={[
+        { name: "name", label: "名称" },
+        { name: "code", label: "编码" },
+        { name: "name1", label: "名称1" },
+        { name: "name2", label: "名称2" },
+      ]}
+      onSearch={() => {}}
+    />
+  );
 };
 
 export default DemoSearch;
