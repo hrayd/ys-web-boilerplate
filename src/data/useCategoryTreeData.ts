ec9a414f014a68d4942759dadf04f600706b970d
artyhacker <artyhacker@outlook.com>
2021-03-31 15:50:01 +0800

    feat: 封装设备类别树数据获取方法

---
+++ src/data/useCategoryTreeData.ts
@@ -0,0 +1,34 @@
+import useSWR from "swr";
+import api from "../api/api";
+import { Category } from "../models/category";
+import { Dict } from "../models/common";
+import YSAxios, { isOk } from "../utils/YSAxios";
+
+const fetcher = async () => {
+  const standardRes = await YSAxios.get(api.standard);
+  const categoryRes = await YSAxios.get(api.category);
+  if (isOk(standardRes) && isOk(categoryRes) && standardRes.data.length) {
+    return [
+      ...standardRes.data.map((d: Dict) => ({
+        ...d,
+        pid: null,
+        pId: null,
+        id: `${d.id}`,
+        title: d.name,
+      })),
+      ...categoryRes.data.map((c: Category) => ({
+        ...c,
+        pId: c.pid,
+        title: c.name,
+      })),
+    ];
+  }
+  return [];
+};
+
+const useCategoryTreeData = () => {
+  const { data, error } = useSWR<Category[]>(api.category, fetcher);
+  return { data, error };
+};
+
+export default useCategoryTreeData;
