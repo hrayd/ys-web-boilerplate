a622f8869960a3a2d2c8dd6cea8f39e7bbb2ad5f
artyhacker <artyhacker@outlook.com>
2021-04-04 17:16:14 +0800

    docs: 优化路由配置文件注释说明

--- src/configs/routeConfigs.ts
+++ src/configs/routeConfigs.ts
@@ -1,24 +1,27 @@
-/** 路由配置
- * 使用延迟加载方式引入：
- * const component = lazy(() => import(PATH_TO_COMPONENT));
+/**
+ * 路由-菜单配置文件
+ * @author donghui
  */
 import { ComponentClass, FC, lazy } from "react";
-
-// const Empty = lazy(() => import("../components/Empty"));
+// 使用延迟加载的方式引入组件
 const Demo = lazy(() => import("../components/Demo"));
 const BasicForm = lazy(() => import("../components/DemoForm/BasicForm"));
 const StepForm = lazy(() => import("../components/DemoForm/StepForm"));
 const ComplexForm = lazy(() => import("../components/DemoForm/ComplexForm"));
 
-/** 页面/路由配置 */
+// 主页路径
+export const HOME_PATH = "/page1";
+
+/**
+ * 页面/路由配置
+ * 多级菜单可根据业务需求调整数据结构，但考虑到后续权限控制的实现，推荐将所有菜单项平铺，通过字段逻辑来进行分组或嵌套
+ */
 const routeConfigs: RouterConfigItem[] = [
   { path: "page1", component: Demo },
   { path: "page2", component: BasicForm },
   { path: "page3", component: StepForm },
   { path: "page4", component: ComplexForm },
 ];
-
-export const HOME_PATH = "/page1";
 
 export default routeConfigs;
 
