2b1c429e3d7468ae23f21a0c5deef0bb652e6856
artyhacker <artyhacker@outlook.com>
2021-05-27 11:20:39 +0800

    feat: 新增优化版网络请求方法request.ts

---
+++ src/utils/request.ts
@@ -0,0 +1,61 @@
+import { message } from "antd";
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import log from "loglevel";
+import { BASE_URL } from "./apiUtils";
+import history from "./history";
+import { setPreLocation } from "./preLocationUtils";
+import { clearToken, getToken } from "./tokenUtils";
+
+const request = axios.create({ baseURL: BASE_URL });
+
+const clearAndBackLogin = () => {
+  clearToken();
+  setPreLocation();
+  history.push("/login");
+};
+
+// Response成功的拦截器，用于处理返回数据
+const onFullFilled = (response: AxiosResponse) => response;
+
+// Response失败的拦截器，用于错误处理
+const onRejected = (err: any): Promise<void> => {
+  log.error("NETWORK REJECTED: ", `${err}`);
+  let msg = "操作失败";
+  if (err.response && err.response.data && err.response.data.code) {
+    const { code, message } = err.response.data;
+    msg = `错误 ${code}: ${message || "Unknown"}`;
+  }
+  if (err.response) {
+    switch (err.response.status) {
+      case 401:
+        msg = "请重新登录";
+        clearAndBackLogin();
+        return new Promise(() => {});
+      default:
+        break;
+    }
+  }
+  message.error(msg);
+  return Promise.reject(err);
+};
+
+// Request拦截器
+const requestOnFullFilled = (
+  request: AxiosRequestConfig
+): AxiosRequestConfig | Promise<AxiosRequestConfig> => {
+  // 添加token
+  const token = getToken();
+  if (token) {
+    return Object.assign(request, {
+      headers: { ...request.headers, Authorization: token },
+    });
+  }
+  message.info("请重新登录");
+  clearAndBackLogin();
+  return new Promise(() => {});
+};
+
+request.interceptors.request.use(requestOnFullFilled);
+request.interceptors.response.use(onFullFilled, onRejected);
+
+export default request;
