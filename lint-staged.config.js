a594c692fdba69256e4de2d83cdc278fb476e94c
artyhacker <artyhacker@outlook.com>
2021-07-09 10:58:44 +0800

    build: config lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  "src/**/*.{js,vue}": ["eslint --fix", "git add"],
-  "src/**/*.{vue,html,css,scss,sass}": ["stylelint --fix", "git add"],
-  "src/**/*.{js,vue,html,css,scss,sass}": [
+  "src/**/*.{js,jsx,ts,tsx}": ["eslint --fix", "git add"],
+  "src/**/*.{jsx,tsx,html,css,scss,sass}": ["stylelint --fix", "git add"],
+  "src/**/*.{js,jsx,tsx,html,css,scss,sass}": [
     "prettier-eslint --write",
     "git add",
   ],
