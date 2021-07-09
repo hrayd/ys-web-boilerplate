48dc1d175f165b1d9347bb1f2ef00c01475e0f8f
artyhacker <artyhacker@outlook.com>
2021-07-09 10:57:13 +0800

    build: config prettier, lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["react-app"],
+  extends: ["react-app", "plugin:prettier/recommended"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
