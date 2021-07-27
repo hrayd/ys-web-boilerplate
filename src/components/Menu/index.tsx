eb665868d5ba9f526138b5cb63509f2ede9e2066
artyhacker <artyhacker@outlook.com>
2021-07-27 16:40:47 +0800

    feat: 菜单栏使用默认trigger

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,9 +1,8 @@
 /** 左侧菜单 */
 import { FC, useMemo, useState } from "react";
 import { Link, RouteComponentProps, withRouter } from "react-router-dom";
-import styled from "styled-components";
 import routeConfigs from "../../configs/routeConfigs";
-import { Menu as AntdMenu, Button } from "antd";
+import { Menu as AntdMenu } from "antd";
 import { useTranslation } from "react-i18next";
 import Sider from "antd/lib/layout/Sider";
 import { DoubleLeftOutlined, DoubleRightOutlined } from "@ant-design/icons";
@@ -29,54 +28,41 @@ const Menu: FC<RouteComponentProps> = ({ location }) => {
     });
   }, [t]);
 
-  return (
-    <>
-      <Sider
-        collapsed={collapsed}
-        collapsible
-        onCollapse={setCollapsed}
-        collapsedWidth={0}
-        theme="light"
-        trigger={null}
-      >
-        <AntdMenu
-          mode="inline"
-          selectedKeys={[activity]}
-          style={{ height: "100%" }}
-        >
-          {menus}
-          {collapsed ? null : (
-            <StyledTrigger
-              onClick={() => setCollapsed((prev) => !prev)}
-              title={t("closeMenu")}
-            >
-              <DoubleLeftOutlined />
-            </StyledTrigger>
-          )}
-        </AntdMenu>
-      </Sider>
-      {collapsed ? (
-        <Button
-          icon={<DoubleRightOutlined />}
+  const trigger = useMemo(
+    () =>
+      collapsed ? (
+        <DoubleRightOutlined
+          style={{ color: "#2196F3" }}
           title={t("openMenu")}
-          onClick={() => setCollapsed(false)}
-          style={{ position: "absolute", top: "8em" }}
-          type="primary"
         />
-      ) : null}
-    </>
+      ) : (
+        <DoubleLeftOutlined
+          style={{ color: "#2196F3" }}
+          title={t("closeMenu")}
+        />
+      ),
+    [collapsed, t]
+  );
+
+  return (
+    <Sider
+      collapsed={collapsed}
+      collapsible
+      onCollapse={setCollapsed}
+      collapsedWidth={0}
+      theme="light"
+      trigger={trigger}
+      breakpoint="lg"
+    >
+      <AntdMenu
+        mode="inline"
+        selectedKeys={[activity]}
+        style={{ height: "100%" }}
+      >
+        {menus}
+      </AntdMenu>
+    </Sider>
   );
 };
 
 export default withRouter(Menu);
-
-const StyledTrigger = styled.div`
-  position: absolute;
-  height: 3em;
-  line-height: 3em;
-  text-align: center;
-  bottom: 0;
-  width: 100%;
-  background-color: rgba(0, 0, 0, 0.1);
-  cursor: pointer;
-`;
